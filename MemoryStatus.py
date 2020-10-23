0a3b7dc47f8ce945db3579b9fc2410d819f88a2c
alichamouda <ali.c.hamouda@gmail.com>
2020-10-23 15:13:18 +0100

    Changed return value to JSON

--- MemoryStatus.py
+++ MemoryStatus.py
@@ -1,12 +1,17 @@
 import psutil
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import os
 
 my_memory_application = Flask(__name__)
 
 @my_memory_application.route('/metrics')
 def getMemoryPercentage():
-   return "{ memory_usage: "+str(psutil.virtual_memory().percent)+"}"
+   response = my_memory_application.response_class(
+        response=json.dumps("{ memory_usage: "+str(psutil.virtual_memory().percent)+"}"),
+        status=200,
+        mimetype='application/json'
+    )
+   return response
 
 @my_memory_application.route('/')
 def getHome():
