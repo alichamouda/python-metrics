ec16dbc29e0f759cd8d677113a7feb7b640c86c7
alichamouda <ali.c.hamouda@gmail.com>
2020-10-23 14:22:39 +0100

    changed all routes to templates

--- MemoryStatus.py
+++ MemoryStatus.py
@@ -6,11 +6,11 @@ my_memory_application = Flask(__name__)
 
 @my_memory_application.route('/metrics')
 def getMemoryPercentage():
-   return 'Memory Percentage is ' + str(psutil.virtual_memory().percent)+'%'
+   return render_template("ram_metrics.html",value=str(psutil.virtual_memory().percent))
 
 @my_memory_application.route('/')
 def getHome():
-   return render_template("ram_preview.html")
+   return render_template("welcome.html")
 
 if __name__ == '__main__':
    my_memory_application.run(port='8080')
